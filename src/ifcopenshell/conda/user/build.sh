5f036426f00bbebb34299cf1b96a50e49a346860
krande <kristoffer_andersen@outlook.com>
2022-03-17 19:42:44 +0100

    Test using macOS cmake instructions from readme

--- src/ifcopenshell/conda/user/build.sh
+++ src/ifcopenshell/conda/user/build.sh
@@ -3,41 +3,53 @@
 declare -a CMAKE_PLATFORM_FLAGS
 
 if [ `uname` == Darwin ]; then
-    export CFLAGS="$CFLAGS   -Wl,-flat_namespace,-undefined,suppress"
-    export CXXFLAGS="$CXXFLAGS -Wl,-flat_namespace,-undefined,suppress"
-    export LDFLAGS="$LDFLAGS  -Wl,-flat_namespace,-undefined,suppress"
+  export CFLAGS="$CFLAGS   -Wl,-flat_namespace,-undefined,suppress"
+  export CXXFLAGS="$CXXFLAGS -Wl,-flat_namespace,-undefined,suppress"
+  export LDFLAGS="$LDFLAGS  -Wl,-flat_namespace,-undefined,suppress"
+
+  cmake ./cmake -DOCC_LIBRARY_DIR=$PREFIX/lib \
+      -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
+      -DCOLLADA_SUPPORT=0 \
+      -DCGAL_INCLUDE_DIR=$PREFIX/include \
+      -DGMP_LIBRARY_DIR=$PREFIX/lib \
+      -DMPFR_LIBRARY_DIR=$PREFIX/lib \
+
+  make -j -lboost_options
+
 else
     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+    cmake -G Ninja \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      ${CMAKE_PLATFORM_FLAGS[@]} \
+     -DCMAKE_PREFIX_PATH=$PREFIX \
+     -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+     -DPython3_FIND_STRATEGY=LOCATION \
+     -DPython3_FIND_FRAMEWORK=NEVER \
+     -DGMP_LIBRARY_DIR=$PREFIX/lib \
+     -DMPFR_LIBRARY_DIR=$PREFIX/lib \
+     -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
+     -DOCC_LIBRARY_DIR=$PREFIX/lib \
+     -DHDF5_SUPPORT:BOOL=ON \
+     -DHDF5_INCLUDE_DIR=$PREFIX/include \
+     -DHDF5_LIBRARY_DIR=$PREFIX/lib \
+     -DJSON_INCLUDE_DIR=$PREFIX/include \
+     -DCGAL_INCLUDE_DIR=$PREFIX/include \
+     -DCOLLADA_SUPPORT:BOOL=OFF \
+     -DBUILD_EXAMPLES:BOOL=OFF \
+     -DIFCXML_SUPPORT:BOOL=OFF \
+     -DBUILD_CONVERT:BOOL=ON \
+     -DBUILD_IFCPYTHON:BOOL=ON \
+     -DBUILD_IFCGEOM:BOOL=ON \
+     -DBUILD_GEOMSERVER:BOOL=OFF \
+     -DIFCXML_SUPPORT:BOOL=OFF \
+     -DBOOST_USE_STATIC_LIBS:BOOL=OFF \
+     ./cmake
+
+    ninja
+
+    ninja install
+
 fi
 
-cmake -G Ninja \
- -DCMAKE_BUILD_TYPE=Release \
- -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  ${CMAKE_PLATFORM_FLAGS[@]} \
- -DCMAKE_PREFIX_PATH=$PREFIX \
- -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
- -DPython3_FIND_STRATEGY=LOCATION \
- -DPython3_FIND_FRAMEWORK=NEVER \
- -DGMP_LIBRARY_DIR=$PREFIX/lib \
- -DMPFR_LIBRARY_DIR=$PREFIX/lib \
- -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
- -DOCC_LIBRARY_DIR=$PREFIX/lib \
- -DHDF5_SUPPORT:BOOL=ON \
- -DHDF5_INCLUDE_DIR=$PREFIX/include \
- -DHDF5_LIBRARY_DIR=$PREFIX/lib \
- -DJSON_INCLUDE_DIR=$PREFIX/include \
- -DCGAL_INCLUDE_DIR=$PREFIX/include \
- -DCOLLADA_SUPPORT:BOOL=OFF \
- -DBUILD_EXAMPLES:BOOL=OFF \
- -DIFCXML_SUPPORT:BOOL=OFF \
- -DBUILD_CONVERT:BOOL=ON \
- -DBUILD_IFCPYTHON:BOOL=ON \
- -DBUILD_IFCGEOM:BOOL=ON \
- -DBUILD_GEOMSERVER:BOOL=OFF \
- -DIFCXML_SUPPORT:BOOL=OFF \
- -DBOOST_USE_STATIC_LIBS:BOOL=OFF \
- ./cmake
 
-ninja
-
-ninja install
