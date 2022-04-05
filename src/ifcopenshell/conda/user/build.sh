37b8842896593db3f186ff61f8e4fe743343bc42
krande <kristoffer_andersen@outlook.com>
2022-04-05 19:33:33 +0200

    minor changes in build and conda config yaml

--- src/ifcopenshell/conda/user/build.sh
+++ src/ifcopenshell/conda/user/build.sh
@@ -12,21 +12,21 @@ if [ `uname` == Darwin ]; then
 
   cmake -G Ninja \
    -DCMAKE_BUILD_TYPE=Release \
-   -DCMAKE_INSTALL_PREFIX=$PREFIX \
+   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     ${CMAKE_PLATFORM_FLAGS[@]} \
-   -DCMAKE_PREFIX_PATH=$PREFIX \
-   -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+   -DCMAKE_PREFIX_PATH="$PREFIX" \
+   -DCMAKE_SYSTEM_PREFIX_PATH="$PREFIX" \
    -DPython3_FIND_STRATEGY=LOCATION \
    -DPython3_FIND_FRAMEWORK=NEVER \
-   -DGMP_LIBRARY_DIR=$PREFIX/lib \
-   -DMPFR_LIBRARY_DIR=$PREFIX/lib \
-   -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
-   -DOCC_LIBRARY_DIR=$PREFIX/lib \
+   -DGMP_LIBRARY_DIR="$PREFIX"/lib \
+   -DMPFR_LIBRARY_DIR="$PREFIX"/lib \
+   -DOCC_INCLUDE_DIR="$PREFIX"/include/opencascade \
+   -DOCC_LIBRARY_DIR="$PREFIX"/lib \
    -DHDF5_SUPPORT:BOOL=ON \
-   -DHDF5_INCLUDE_DIR=$PREFIX/include \
-   -DHDF5_LIBRARY_DIR=$PREFIX/lib \
-   -DJSON_INCLUDE_DIR=$PREFIX/include \
-   -DCGAL_INCLUDE_DIR=$PREFIX/include \
+   -DHDF5_INCLUDE_DIR="$PREFIX"/include \
+   -DHDF5_LIBRARY_DIR="$PREFIX"/lib \
+   -DJSON_INCLUDE_DIR="$PREFIX"/include \
+   -DCGAL_INCLUDE_DIR="$PREFIX"/include \
    -DCOLLADA_SUPPORT:BOOL=OFF \
    -DBUILD_EXAMPLES:BOOL=OFF \
    -DIFCXML_SUPPORT:BOOL=OFF \
@@ -36,35 +36,37 @@ if [ `uname` == Darwin ]; then
    -DBUILD_GEOMSERVER:BOOL=OFF \
    -DIFCXML_SUPPORT:BOOL=OFF \
    -DBOOST_USE_STATIC_LIBS:BOOL=OFF \
+   -DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}" \
    ./cmake
 
   # Build and install
   cmake --build . -- install
-#
-#  # fix rpaths
-#  for lib in $(ls $SP_DIR/ifcopenshell/_*.so); do
-#    install_name_tool -rpath $PREFIX/lib @loader_path/../../../ $lib
-#  done
+
+  # fix rpaths
+  # shellcheck disable=SC2045
+  for lib in $(ls "$SP_DIR"/ifcopenshell/_*.so); do
+    install_name_tool -rpath "$PREFIX"/lib @loader_path/../../../ "$lib"
+  done
 
 else
   cmake -G Ninja \
    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
    -DCMAKE_BUILD_TYPE=Release \
-   -DCMAKE_INSTALL_PREFIX=$PREFIX \
+   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     ${CMAKE_PLATFORM_FLAGS[@]} \
-   -DCMAKE_PREFIX_PATH=$PREFIX \
-   -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+   -DCMAKE_PREFIX_PATH="$PREFIX" \
+   -DCMAKE_SYSTEM_PREFIX_PATH="$PREFIX" \
    -DPython3_FIND_STRATEGY=LOCATION \
    -DPython3_FIND_FRAMEWORK=NEVER \
-   -DGMP_LIBRARY_DIR=$PREFIX/lib \
-   -DMPFR_LIBRARY_DIR=$PREFIX/lib \
-   -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
-   -DOCC_LIBRARY_DIR=$PREFIX/lib \
+   -DGMP_LIBRARY_DIR="$PREFIX"/lib \
+   -DMPFR_LIBRARY_DIR="$PREFIX"/lib \
+   -DOCC_INCLUDE_DIR="$PREFIX"/include/opencascade \
+   -DOCC_LIBRARY_DIR="$PREFIX"/lib \
    -DHDF5_SUPPORT:BOOL=ON \
-   -DHDF5_INCLUDE_DIR=$PREFIX/include \
-   -DHDF5_LIBRARY_DIR=$PREFIX/lib \
-   -DJSON_INCLUDE_DIR=$PREFIX/include \
-   -DCGAL_INCLUDE_DIR=$PREFIX/include \
+   -DHDF5_INCLUDE_DIR="$PREFIX"/include \
+   -DHDF5_LIBRARY_DIR="$PREFIX"/lib \
+   -DJSON_INCLUDE_DIR="$PREFIX"/include \
+   -DCGAL_INCLUDE_DIR="$PREFIX"/include \
    -DCOLLADA_SUPPORT=0 \
    -DBUILD_EXAMPLES:BOOL=OFF \
    -DIFCXML_SUPPORT:BOOL=OFF \
