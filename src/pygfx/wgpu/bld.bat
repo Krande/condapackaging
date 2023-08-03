b2bc5f9e461874fe8ce4bbbe1167b79a1a5d76af
krande <kristoffer_andersen@outlook.com>
2023-08-03 21:33:30 +0200

    include native wgpu lib

---
+++ src/pygfx/wgpu/bld.bat
@@ -0,0 +1,4 @@
+copy %RECIPE_DIR%\\download-wgpu-native.py .
+copy %RECIPE_DIR%\\codegen_report.md "wgpu\\resources\\codegen_report.md"
+python download-wgpu-native.py
+python -m pip install . -vv
---
+++ src/pygfx/wgpu/build.sh
@@ -0,0 +1,4 @@
+cp %RECIPE_DIR%/download-wgpu-native.py .
+cp %RECIPE_DIR%\\codegen_report.md "wgpu\\resources\\codegen_report.md"
+python download-wgpu-native.py
+python -m pip install . -vv
