373ac9487f783f4c2d2f87a89513a7c91c4a2694
krande <kristoffer_andersen@outlook.com>
2023-08-06 16:59:53 +0200

    add code aster

---
+++ src/code_aster/code_aster/config/wafcfg_conda.py
@@ -0,0 +1,41 @@
+import os
+
+
+def configure(self):
+    opts = self.options
+
+    print('Running Conda Configuration')
+
+    include_dir = os.getenv('CONDA_INCLUDE_PATH')
+    lib_dir = os.getenv('CONDA_LIBRARY_PATH')
+    conda_prefix = os.getenv('CONDA_PREFIX')
+    recipe_dir = os.getenv('RECIPE_DIR')
+
+    self.env['ADDMEM'] = 600
+
+    self.env.INCLUDES_BOOST = include_dir
+    self.env.LIBPATH_BOOST = [lib_dir]
+    self.env.LIB_BOOST = ['libboost_python310']
+    self.env.TFELHOME = [conda_prefix, conda_prefix + '/bin']
+
+    self.env.LIBPATH_METIS = [lib_dir, ]
+    self.env.INCLUDES_METIS = include_dir
+
+    self.env.WAFBUILD_ENV = [recipe_dir + '/config/dummy.env', conda_prefix]
+
+    self.env.append_value('LIBPATH', [
+        lib_dir,
+    ])
+
+    self.env.append_value('INCLUDES', [
+        include_dir,
+    ])
+
+    # to fail if not found
+    opts.enable_hdf5 = True
+    opts.enable_med = True
+    opts.enable_metis = True
+    opts.enable_mumps = True
+    opts.enable_scotch = True
+    opts.enable_mfront = True
+    print('Conda Configuration Complete')
