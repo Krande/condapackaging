e1acca718c27cfd32b801922fd8649565fe793f4
krande <kristoffer_andersen@outlook.com>
2023-08-08 15:32:23 +0200

    demonstrate a working python 3.9 compilation.
    Next up. Figure out why 3.10 & 3.11 isnt working and run tests

--- src/code_aster/code_aster/build.sh
+++ src/code_aster/code_aster/build.sh
@@ -18,17 +18,21 @@ export INCLUDES="$PREFIX/include $INCLUDES"
 export LIBPATH_METIS="$PREFIX/metis-aster/lib"
 export INCLUDES_METIS="$PREFIX/metis-aster/include"
 
+#export ASTERLIBDIR="$PREFIX/lib"
+#export PYTHONPATH="$SP_DIR:$SRC_DIR:$PYTHONPATH"
+#export LD_LIBRARY_PATH="$SRC_DIR/deps/devtools/lib:$LD_LIBRARY_PATH"
+#export PATH="$SRC_DIR/deps/devtools/bin:$PATH"
+
 # Install for standard sequential
 ./waf \
   --use-config=wafcfg_conda \
   --use-config-dir="$RECIPE_DIR"/config \
-  --prefix="$PREFIX" \
-  --pythondir="$PREFIX/lib/aster" \
+  --prefix=$PREFIX \
   --embed-metis \
-  --with-data=data \
   --without-hg \
   configure
-#./waf install -v
+./waf install
+
 #
 #find $PREFIX -name "profile.sh" -exec sed -i 's/PYTHONHOME=/#PYTHONHOME=/g' {} \;
 #find $PREFIX -name "profile.sh" -exec sed -i 's/export PYTHONHOME/#export PYTHONHOME/g' {} \;
