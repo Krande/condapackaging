94927ca7863bb99441ab54f6267673fde807c446
krande <kristoffer_andersen@outlook.com>
2023-08-08 13:24:05 +0200

    still not a working installation

--- src/code_aster/code_aster/build.sh
+++ src/code_aster/code_aster/build.sh
@@ -3,7 +3,6 @@
 export CONDA_INCLUDE_PATH="$CONDA_PREFIX/include"
 export CONDA_LIBRARY_PATH="$CONDA_PREFIX/lib"
 
-cp $RECIPE_DIR/config/wafcfg_conda.py .
 cp $RECIPE_DIR/bld/* .
 
 chmod +x ./install_asrun.sh
@@ -13,8 +12,8 @@ chmod +x ./install_metis.sh
 ./install_metis.sh
 
 export TFELHOME=$PREFIX
-export LIBPATH="$PREFIX/metis-aster/lib $PREFIX/lib $LIBPATH"
-export INCLUDES="$PREFIX/metis-aster/include $PREFIX/include $INCLUDES"
+export LIBPATH="$PREFIX/lib $LIBPATH"
+export INCLUDES="$PREFIX/include $INCLUDES"
 
 export LIBPATH_METIS="$PREFIX/metis-aster/lib"
 export INCLUDES_METIS="$PREFIX/metis-aster/include"
@@ -22,15 +21,15 @@ export INCLUDES_METIS="$PREFIX/metis-aster/include"
 # Install for standard sequential
 ./waf \
   --use-config=wafcfg_conda \
-  --prefix="${PREFIX}" \
-  --libdir="${PREFIX}/lib" \
-  --pythondir="${PREFIX}/lib/aster" \
+  --use-config-dir="$RECIPE_DIR"/config \
+  --prefix="$PREFIX" \
+  --pythondir="$PREFIX/lib/aster" \
   --embed-metis \
   --with-data=data \
   --without-hg \
   configure
-./waf install -v
-
-find $PREFIX -name "profile.sh" -exec sed -i 's/PYTHONHOME=/#PYTHONHOME=/g' {} \;
-find $PREFIX -name "profile.sh" -exec sed -i 's/export PYTHONHOME/#export PYTHONHOME/g' {} \;
-
+#./waf install -v
+#
+#find $PREFIX -name "profile.sh" -exec sed -i 's/PYTHONHOME=/#PYTHONHOME=/g' {} \;
+#find $PREFIX -name "profile.sh" -exec sed -i 's/export PYTHONHOME/#export PYTHONHOME/g' {} \;
+#
