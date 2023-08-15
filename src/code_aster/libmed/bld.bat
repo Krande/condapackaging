8a3b03e269cbdd797b5ae37255f24a845c1d84ff
krande <kristoffer_andersen@outlook.com>
2023-08-15 10:08:29 +0200

    update libmed compilation to latest

--- src/code_aster/libmed/bld.bat
+++ src/code_aster/libmed/bld.bat
@@ -1,22 +1,30 @@
-mkdir build -p
-cd build
 
 cmake -G "Ninja" ^
+      -Wno-dev ^
       -D CMAKE_BUILD_TYPE=Release ^
-      -D CMAKE_PREFIX_PATH:FILEPATH=%PREFIX% ^
-      -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
-      -D HDF5_LIBRARIES:FILEPATH=%LIBRARY_PREFIX%\lib\libhdf5.lib;%LIBRARY_PREFIX%\lib\zlib.lib ^
-      -D HDF5_ROOT_DIR:FILEPATH=%LIBRARY_PREFIX% ^
+      -D "CMAKE_PREFIX_PATH:FILEPATH=%PREFIX%" ^
+      -D "CMAKE_INSTALL_PREFIX:FILEPATH=%PREFIX%" ^
+      -D "HDF5_ROOT_DIR:FILEPATH=%LIBRARY_PREFIX%" ^
       -D MEDFILE_INSTALL_DOC=OFF ^
       -D MEDFILE_BUILD_PYTHON=ON ^
-      -D PYTHON_INSTALL_DIR:FILEPATH=%SP_DIR% ^
-      ..
+      -D "PYTHON_LIBRARY:FILEPATH=%PREFIX%\libs\python%CONDA_PY%.lib" ^
+      -D "PYTHON_INCLUDE_DIR:FILEPATH=%PREFIX%\include" -S . -B build
 
 if errorlevel 1 exit 1
-ninja install
+cmake --build .\build --config Release
 if errorlevel 1 exit 1
+cmake --install .\build
+
+DEL /q %PREFIX%\bin\mdump %PREFIX%\bin\xmdump
+COPY /y %PREFIX%\bin\mdump4 %PREFIX%\bin\mdump
+COPY /y %PREFIX%\bin\xmdump4 %PREFIX%\bin\xmdump
+MKDIR %SP_DIR%\med
+MOVE %PREFIX%\lib\python%PY_VER%\site-packages\med %SP_DIR%\med
+MOVE %PREFIX%\lib\medC.* %LIBRARY_BIN%
+
+
 REM ninja test
 REM if errorlevel 1 exit 1
 
-move %LIBRARY_PREFIX%\lib\medC.dll %LIBRARY_PREFIX%\bin
-move %LIBRARY_PREFIX%\lib\medimport.dll %LIBRARY_PREFIX%\bin
+REM move %LIBRARY_PREFIX%\lib\medC.dll %LIBRARY_PREFIX%\bin
+REM move %LIBRARY_PREFIX%\lib\medimport.dll %LIBRARY_PREFIX%\bin
--- src/code_aster/libmed/build.sh
+++ src/code_aster/libmed/build.sh
@@ -1,21 +1,33 @@
 export FCFLAGS="-w -fallow-argument-mismatch -O2"
 export FFLAGS="-w -fallow-argument-mismatch -O2"
 
-#mkdir build -p
-#cd build 
+echo "PYTHON=${PYTHON}"
+echo "PY_VER=${PY_VER}"
+echo "SP_DIR=${SP_DIR}"
+echo "STDLIB_DIR=${STDLIB_DIR}"
 
-#cmake -G "Ninja"  \
-#      -D CMAKE_BUILD_TYPE=Release \
-#      -D CMAKE_INSTALL_PREFIX=$PREFIX \
-#      -D MEDFILE_INSTALL_DOC=OFF \
-#      -D MEDFILE_BUILD_PYTHON=ON \
-#      ..
+PYTHON_INCLUDE_DIR=$(${PYTHON} -c 'import sysconfig;print("{0}".format(sysconfig.get_path("platinclude")))')
+echo "PYTHON_INCLUDE_DIR=${PYTHON_INCLUDE_DIR}"
+#PYTHON_LIBRARY=$(${PYTHON} -c 'import sysconfig;print("{0}/{1}".format(*map(sysconfig.get_config_var, ("LIBDIR", "LDLIBRARY"))))')
+#echo "PYTHON_LIBRARY=${PYTHON_LIBRARY}"
 
-#ninja install
-#ninja test
+VERBOSE=1 cmake \
+          -Wno-dev \
+          -D CMAKE_BUILD_TYPE=Release \
+          -D "CMAKE_PREFIX_PATH=${PREFIX}" \
+          -D "CMAKE_INSTALL_PREFIX=${PREFIX}" \
+          -D CMAKE_FIND_FRAMEWORK=NEVER \
+          -D "HDF5_ROOT_DIR=${LIBRARY_PREFIX}" \
+          -D MEDFILE_INSTALL_DOC=OFF \
+          -D MEDFILE_BUILD_PYTHON=ON \
+          -D "PYTHON_LIBRARY=${PREFIX}/lib" \
+          -D "PYTHON_INCLUDE_DIR=${PYTHON_INCLUDE_DIR}" -S . -B build
 
-./configure --prefix=$PREFIX --with-f90 --with-hdf5=$PREFIX
-make
-make check 
-make installcheck
-make install
+cmake --build ./build --config Release
+cmake --install ./build
+
+#./configure --prefix=$PREFIX --with-f90 --with-hdf5=$PREFIX
+#make
+#make check
+#make installcheck
+#make install
