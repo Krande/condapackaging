63424c7b70f1045142455cc3beefd70375352335
krande <kristoffer_andersen@outlook.com>
2023-08-17 10:25:49 +0200

    update upload script to look for .conda files

--- src/code_aster/libmed/build.sh
+++ src/code_aster/libmed/build.sh
@@ -1,8 +1,8 @@
-export FCFLAGS="-w -fallow-argument-mismatch -O2 -fdefault-integer-8 ${FCFLAGS}"
-export FFLAGS="-w -fallow-argument-mismatch -O2 -fdefault-integer-8 ${FFLAGS}"
+export FCFLAGS="-fdefault-integer-8 ${FCFLAGS}"
+export FFLAGS="-fdefault-integer-8 ${FFLAGS}"
 
-./configure --prefix=$PREFIX --with-f90 --with-hdf5=$PREFIX
+./configure --prefix="$PREFIX" --with-f90 --with-hdf5="$PREFIX"
 make
 make install
 
-rm -rf ${PREFIX}/share/doc/med
+rm -rf "${PREFIX}/share/doc/med"
