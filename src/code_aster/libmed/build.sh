fc0194bf1ab4c0d5a54156bc100a162179716724
krande <kristoffer_andersen@outlook.com>
2023-08-18 12:09:25 +0200

    Update build configuration for libmed
    Increase build number in the recipe.yaml from 3 to 4 and updated the configuration options in build.sh script to include SWIG.
    This is necessary to integrate the SWIG interface generation system, which will allow for better support across multiple prescribed languages. The build number is increased to distinguish from previous builds.

--- src/code_aster/libmed/build.sh
+++ src/code_aster/libmed/build.sh
@@ -1,7 +1,7 @@
 export FCFLAGS="-fdefault-integer-8 ${FCFLAGS}"
 export FFLAGS="-fdefault-integer-8 ${FFLAGS}"
 
-./configure --prefix="$PREFIX" --with-f90 --with-hdf5="$PREFIX"
+./configure --with-swig=yes --prefix="$PREFIX" --with-f90 --with-hdf5="$PREFIX"
 make
 make install
 
