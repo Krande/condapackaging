968c2f789f677ff4a7c9f0da49f9607b6f19e954
krande <kristoffer_andersen@outlook.com>
2023-08-18 16:09:34 +0200

    update fail scan utility

--- src/code_aster/scan_failed.py
+++ src/code_aster/scan_failed.py
@@ -3,16 +3,19 @@ import pathlib
 resutest_dir = pathlib.Path(__file__).parent / "resutest"
 last_failed = resutest_dir / "Testing/Temporary/LastTestsFailed.log"
 aster_ver = '16.4.2'
-num = 0
+
 
 fail_messages = [
     "Le fichier xmgrace n'existe pas",
     "La commande CALC_ERC_DYN ne peut fonctionner que sur des maillages ne contenant que des SEG2",
-    "tldlg3.F90, ligne 356"
+    "tldlg3.F90, ligne 356",
+    "Échec de la factorisation de Cholesky: la matrice n'est pas symétrique définie positive"
 ]
 
-failed_due_to = []
+
 for fail_msg in fail_messages:
+    num = 0
+    failed_due_to = []
     with open(last_failed) as f:
         for line in f:
             failed = line.split(f'{aster_ver}_')[-1].strip()
@@ -22,5 +25,5 @@ for fail_msg in fail_messages:
                     num += 1
                     failed_due_to.append(failed)
 
-    print(f'{num} failed to du {fail_msg}')
+    print(f'{num} failures containing "{fail_msg}"')
     print('|'.join(failed_due_to))
