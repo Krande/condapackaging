ef76be57b62e3a28297b644fd029678a2cd9e138
krande <kristoffer_andersen@outlook.com>
2023-08-19 20:37:41 +0200

    further work on win

--- src/code_aster/libmed/bld.bat
+++ src/code_aster/libmed/bld.bat
@@ -3,35 +3,40 @@
 mkdir "%SRC_DIR%\deps\config"
 tar -xzvf "%SRC_DIR%\deps\archives\med-4.1.1.tar.gz" -C . --strip-components=1
 
-set FCFLAGS=-fdefault-integer-8 %FCFLAGS%
-set FFLAGS=-fdefault-integer-8 %FFLAGS%
+::set FCFLAGS=-fdefault-integer-8 %FCFLAGS%
+::set FFLAGS=-fdefault-integer-8 %FFLAGS%
 
-set CMAKE_C_COMPILER=%PREFIX%\mingw-w64\bin\gcc.exe
-set CMAKE_CXX_COMPILER=%PREFIX%\mingw-w64\bin\g++.exe
+::set CMAKE_C_COMPILER=%PREFIX%\mingw-w64\bin\gcc.exe
+::set CMAKE_CXX_COMPILER=%PREFIX%\mingw-w64\bin\g++.exe
 set CMAKE_Fortran_COMPILER=%PREFIX%\mingw-w64\bin\gfortran.exe
 
-cmake -G "Ninja" ^
+echo %CC%
+echo %CXX%
+echo %FC%
+
+cmake -G "Ninja" -S .^
       -Wno-dev ^
+      -D MED_MEDINT_TYPE:STRING=long ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D "CMAKE_PREFIX_PATH:FILEPATH=%PREFIX%" ^
       -D "CMAKE_INSTALL_PREFIX:FILEPATH=%PREFIX%" ^
-      -D "HDF5_ROOT_DIR:FILEPATH=%LIBRARY_PREFIX%" ^
+      -D "HDF5_ROOT_DIR:FILEPATH=%LIBRARY_PREFIX%/lib" ^
       -D MEDFILE_INSTALL_DOC=OFF ^
       -D MEDFILE_BUILD_PYTHON=ON ^
       -D "PYTHON_LIBRARY:FILEPATH=%PREFIX%\libs\python%CONDA_PY%.lib" ^
-      -D "PYTHON_INCLUDE_DIR:FILEPATH=%PREFIX%\include" -S . -B build
+      -D "PYTHON_INCLUDE_DIR:FILEPATH=%PREFIX%\include"
 
 if errorlevel 1 exit 1
-cmake --build .\build --config Release
+ninja
 if errorlevel 1 exit 1
-cmake --install .\build
+ninja install
 
-DEL /q %PREFIX%\bin\mdump %PREFIX%\bin\xmdump
-COPY /y %PREFIX%\bin\mdump4 %PREFIX%\bin\mdump
-COPY /y %PREFIX%\bin\xmdump4 %PREFIX%\bin\xmdump
-MKDIR %SP_DIR%\med
-MOVE %PREFIX%\lib\python%PY_VER%\site-packages\med %SP_DIR%\med
-MOVE %PREFIX%\lib\medC.* %LIBRARY_BIN%
+rem DEL /q %PREFIX%\bin\mdump %PREFIX%\bin\xmdump
+rem COPY /y %PREFIX%\bin\mdump4 %PREFIX%\bin\mdump
+rem COPY /y %PREFIX%\bin\xmdump4 %PREFIX%\bin\xmdump
+rem MKDIR %SP_DIR%\med
+rem MOVE %PREFIX%\lib\python%PY_VER%\site-packages\med %SP_DIR%\med
+rem MOVE %PREFIX%\lib\medC.* %LIBRARY_BIN%
 
 
 REM ninja test
