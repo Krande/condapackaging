a82b26e8a07e2f799dda21c64c84ac3e3b3cc3f5
krande <kristoffer_andersen@outlook.com>
2023-08-19 10:03:16 +0200

    compile libmed with "aster" in package hash

--- src/code_aster/libmed/build.sh
+++ src/code_aster/libmed/build.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+mkdir -p "$SRC_DIR/deps/config"
+tar -xzvf "$SRC_DIR/deps/archives/med-4.1.1.tar.gz" -C . --strip-components=1
+
 export FCFLAGS="-fdefault-integer-8 ${FCFLAGS}"
 export FFLAGS="-fdefault-integer-8 ${FFLAGS}"
 
