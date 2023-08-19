992c2011690519a7fa9d29a58cf983d62d1d6da3
krande <kristoffer_andersen@outlook.com>
2023-08-19 14:22:08 +0200

    Update dependencies and environment variables in code_aster
    Dependencies in recipe.yaml have been updated to improve conda package resolution. Made changes to facilitate the usage with WSL2 on windows in code_aster_deactivate.sh and code_aster_activate.sh. This update provides a smoother user experience when installing the package, especially for WSL2 users. Also, reordered sections and removed unnecessary elements in recipe.yaml for better readability.

--- src/code_aster/code_aster/config/code_aster_activate.sh
+++ src/code_aster/code_aster/config/code_aster_activate.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Save current values of environment variables
-echo "$ASTER_LIBDIR" > "$CONDA_PREFIX/.aster_libdir_old"
-echo "$ASTER_DATADIR" > "$CONDA_PREFIX/.aster_datadir_old"
-echo "$ASTER_LOCALEDIR" > "$CONDA_PREFIX/.aster_localedir_old"
-echo "$ASTER_ELEMENTSDIR" > "$CONDA_PREFIX/.aster_elementsdir_old"
+# https://docs.conda.io/projects/conda/en/latest/dev-guide/deep-dives/activation.html
+
 
 export PYTHONPATH="${CONDA_PREFIX}/lib/aster:${PYTHONPATH}"
 export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib/aster:${LD_LIBRARY_PATH}"
@@ -12,6 +9,13 @@ export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib/aster:${LD_LIBRARY_PATH}"
 # Not sure if these really matter
 export C_INCLUDE_PATH="${CONDA_PREFIX}/include/aster:${C_INCLUDE_PATH}"
 export CPLUS_INCLUDE_PATH="${CONDA_PREFIX}/include/aster:${CPLUS_INCLUDE_PATH}"
+
+# Save current values of environment variables
+# Will allow
+#echo "$ASTER_LIBDIR" > "$CONDA_PREFIX/.aster_libdir_old"
+#echo "$ASTER_DATADIR" > "$CONDA_PREFIX/.aster_datadir_old"
+#echo "$ASTER_LOCALEDIR" > "$CONDA_PREFIX/.aster_localedir_old"
+#echo "$ASTER_ELEMENTSDIR" > "$CONDA_PREFIX/.aster_elementsdir_old"
 
 export ASTER_LIBDIR="$CONDA_PREFIX/lib/aster"
 export ASTER_DATADIR="$CONDA_PREFIX/share/aster"
--- src/code_aster/code_aster/config/code_aster_deactivate.sh
+++ src/code_aster/code_aster/config/code_aster_deactivate.sh
@@ -2,17 +2,26 @@
 
 export PYTHONPATH=$(echo $PYTHONPATH | sed "s|$CONDA_PREFIX/lib/aster:||g")
 export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed "s|$CONDA_PREFIX/lib/aster:||g")
+
 export C_INCLUDE_PATH=$(echo $C_INCLUDE_PATH | sed "s|$CONDA_PREFIX/include/aster:||g")
 export CPLUS_INCLUDE_PATH=$(echo $CPLUS_INCLUDE_PATH | sed "s|$CONDA_PREFIX/include/aster:||g")
 
-# Restore original values of environment variables, or unset them if they were not previously set
-export ASTER_LIBDIR=$(cat $CONDA_PREFIX/.aster_libdir_old 2> /dev/null || echo "")
-export ASTER_DATADIR=$(cat $CONDA_PREFIX/.aster_datadir_old 2> /dev/null || echo "")
-export ASTER_LOCALEDIR=$(cat $CONDA_PREFIX/.aster_localedir_old 2> /dev/null || echo "")
-export ASTER_ELEMENTSDIR=$(cat $CONDA_PREFIX/.aster_elementsdir_old 2> /dev/null || echo "")
+# These seem to cause pycharm marking the python interpreter as invalid when used on WSL2 on windows
+## Restore original values of environment variables, or unset them if they were not previously set
+#export ASTER_LIBDIR=$(cat $CONDA_PREFIX/.aster_libdir_old 2> /dev/null || echo "")
+#export ASTER_DATADIR=$(cat $CONDA_PREFIX/.aster_datadir_old 2> /dev/null || echo "")
+#export ASTER_LOCALEDIR=$(cat $CONDA_PREFIX/.aster_localedir_old 2> /dev/null || echo "")
+#export ASTER_ELEMENTSDIR=$(cat $CONDA_PREFIX/.aster_elementsdir_old 2> /dev/null || echo "")
+#
+## Remove temporary files
+#rm -f $CONDA_PREFIX/.aster_libdir_old
+#rm -f $CONDA_PREFIX/.aster_datadir_old
+#rm -f $CONDA_PREFIX/.aster_localedir_old
+#rm -f $CONDA_PREFIX/.aster_elementsdir_old
 
-# Remove temporary files
-rm -f $CONDA_PREFIX/.aster_libdir_old
-rm -f $CONDA_PREFIX/.aster_datadir_old
-rm -f $CONDA_PREFIX/.aster_localedir_old
-rm -f $CONDA_PREFIX/.aster_elementsdir_old
+# Do simply this instead
+
+unset ASTER_LIBDIR
+unset ASTER_DATADIR
+unset ASTER_LOCALEDIR
+unset ASTER_ELEMENTSDIR
