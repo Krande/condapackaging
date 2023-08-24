074a576b47feef0fa8dcd9df7e9f71de6d4b3025
krande <kristoffer_andersen@outlook.com>
2023-08-24 10:51:32 +0200

    "Lock numpy version across packages"
    This commit ensures the same version of numpy (1.22) is used across all packages. Previously, each package could use a different version of numpy, which might lead to inconsistencies or bugs when these packages interact with each other. The changes were made in `conda_build_config.yaml` files, where numpy version was explicitly set to 1.22. Also, the helper function `ensure_consistent_numpy_versions` was added to `modhelper.py` to automate this procedure. Numpy dependencies in `meta.yaml` and `recipe.yaml` files were also updated accordingly.

--- src/code_aster/modhelper.py
+++ src/code_aster/modhelper.py
@@ -9,14 +9,31 @@ yaml.indent(mapping=2, offset=2)
 yaml.preserve_quotes = True  # not necessary for your current input
 yaml.allow_duplicate_keys = True
 
-def change_compilers(compiler_version, root_dir='.'):
-    """Make changes to the compiler versions set in the conda_build_config.yaml files"""
+
+def iter_valid_dirs(root_dir):
     for subdir in pathlib.Path(root_dir).iterdir():
         if not subdir.is_dir():
             continue
         config_file = subdir.joinpath("conda_build_config.yaml")
         if not config_file.exists():
             continue
+        yield config_file
+
+
+def ensure_consistent_numpy_versions(numpy_version, root_dir='.'):
+    for config_file in iter_valid_dirs(root_dir):
+        with open(config_file, 'r') as f:
+            data = yaml.load(f)
+
+        data.setdefault('numpy', [numpy_version])
+
+        with open(config_file, 'w') as f:
+            yaml.dump(data, f)
+
+
+def change_compilers(compiler_version, root_dir='.'):
+    """Make changes to the compiler versions set in the conda_build_config.yaml files"""
+    for config_file in iter_valid_dirs(root_dir):
         with open(config_file, 'r') as f:
             data = yaml.load(f)
 
@@ -31,4 +48,5 @@ def change_compilers(compiler_version, root_dir='.'):
 
 
 if __name__ == '__main__':
-    change_compilers(12)
+    # change_compilers(12)
+    ensure_consistent_numpy_versions('1.22')
