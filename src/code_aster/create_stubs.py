67dfce1baf59adbcf951eff13bd8f7772a4a884c
krande <kristoffer_andersen@outlook.com>
2023-08-26 15:43:02 +0200

    minor update of stubs script

--- src/code_aster/create_stubs.py
+++ src/code_aster/create_stubs.py
@@ -9,6 +9,7 @@ stubs_dir = pathlib.Path(__file__).parent / 'stubs' / 'code_aster-stubs'
 aster_lib = pathlib.Path(conda_prefix) / 'lib' / 'aster' / 'code_aster'
 dummy_lib = './dummy_lib'
 # pybind11_stubgen.main(['--help'])
-
-pybind11_stubgen.main(['code_aster', '--ignore-invalid=all', '--no-setup-py', '--skip-signature-downgrade'])
+args = ['code_aster', '--ignore-invalid=all', '--no-setup-py']
+# args += ['--skip-signature-downgrade']
+pybind11_stubgen.main(args)
 # shutil.copytree('stubs', dummy_lib, dirs_exist_ok=True)
