198294dfe7bdb6ffbb10cec81a582393f426fe9f
krande <kristoffer_andersen@outlook.com>
2023-09-05 17:40:55 +0200

    do not install unused packages in base env

--- src/code_aster/petsc/build.sh
+++ src/code_aster/petsc/build.sh
@@ -29,21 +29,22 @@ export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
 # SHLIB_EXT https://docs.conda.io/projects/conda-build/en/latest/user-guide/environment-variables.html#id2
 if [[ "${PKG_DEBUG}" == "True" ]]; then
   enable_debugging=1
+  export CFLAGS="-g -O0 ${CFLAGS}"
+  export CXXFLAGS="-g -O0 ${CXXFLAGS}"
+  export FCFLAGS="-g -O0 ${FCFLAGS}"
 else
   enable_debugging=0
 fi
 
+export CC=mpicc
+export CXX=mpicxx
+export FC=mpifort
+export F77=mpif77
+export F90=mpif90
+
+echo "python=$(which python)"
 # ensure to have consistent python3 and cython in PATH
-python3 ./configure \
-    CC="mpicc" \
-    CXX="mpicxx" \
-    FC="mpifort" \
-    CFLAGS="$CFLAGS" \
-    CPPFLAGS="$CPPFLAGS" \
-    CXXFLAGS="$CXXFLAGS" \
-    FFLAGS="$FFLAGS" \
-    LDFLAGS="$LDFLAGS" \
-    LIBS="$LIBS" \
+python ./configure \
     --with-debugging=$enable_debugging \
     --with-mpi=1 \
     --with-ssl=0 \
@@ -57,12 +58,12 @@ python3 ./configure \
     --with-python=1 \
     --with-petsc4py=1 \
     --with-shared-libraries \
-    --download-ml=${BUILD_3rdParty}/3rd/pkg-trilinos-ml-${_ML}.tar.gz \
-    --download-sowing=${BUILD_3rdParty}/3rd/sowing_${_SOWING}.tar.gz \
-    --download-hypre=${BUILD_3rdParty}/3rd/hypre_${_HYPRE}.tar.gz \
-    --download-superlu=${BUILD_3rdParty}/3rd/SuperLU_${_SUPERLU}.tar.gz \
-    --download-slepc=${BUILD_3rdParty}/3rd/slepc-${_SPLEC}.tar.gz \
-    --download-hpddm=${BUILD_3rdParty}/3rd/hpddm_${_HPDDM}.tar.gz \
+    --download-ml="${BUILD_3rdParty}/3rd/pkg-trilinos-ml-${_ML}.tar.gz" \
+    --download-sowing="${BUILD_3rdParty}"/3rd/sowing_${_SOWING}.tar.gz \
+    --download-hypre="${BUILD_3rdParty}"/3rd/hypre_${_HYPRE}.tar.gz \
+    --download-superlu="${BUILD_3rdParty}"/3rd/SuperLU_${_SUPERLU}.tar.gz \
+    --download-slepc="${BUILD_3rdParty}"/3rd/slepc-${_SPLEC}.tar.gz \
+    --download-hpddm="${BUILD_3rdParty}"/3rd/hpddm_${_HPDDM}.tar.gz \
     --with-openmp=0 \
     --prefix="${PREFIX}"
 
