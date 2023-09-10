7cab0f9f10f08b4bb2c68d477780bf004a483e16
krande <kristoffer_andersen@outlook.com>
2023-09-10 18:12:21 +0200

    add more activation scripts

--- src/code_aster/code_aster/config/code_aster_activate.sh
+++ src/code_aster/code_aster/config/code_aster_activate.sh
@@ -2,12 +2,12 @@
 
 # https://docs.conda.io/projects/conda/en/latest/dev-guide/deep-dives/activation.html
 
-export PYTHONPATH="${CONDA_PREFIX}/lib/aster:${PYTHONPATH}"
-export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib/aster:${LD_LIBRARY_PATH}"
+export PYTHONPATH="${CONDA_PREFIX}/lib/aster:${CONDA_PREFIX}/lib:${PYTHONPATH}"
+export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib/aster:${CONDA_PREFIX}/lib/petsc4py/lib:${LD_LIBRARY_PATH}"
 
 # Not sure if these really matter
-export C_INCLUDE_PATH="${CONDA_PREFIX}/include/aster:${C_INCLUDE_PATH}"
-export CPLUS_INCLUDE_PATH="${CONDA_PREFIX}/include/aster:${CPLUS_INCLUDE_PATH}"
+export C_INCLUDE_PATH="${CONDA_PREFIX}/include/aster:${CONDA_PREFIX}/lib/petsc4py/include:${C_INCLUDE_PATH}"
+export CPLUS_INCLUDE_PATH="${CONDA_PREFIX}/include/aster:${CONDA_PREFIX}/lib/petsc4py/include:${CPLUS_INCLUDE_PATH}"
 
 # Save current values of environment variables
 # Will allow
