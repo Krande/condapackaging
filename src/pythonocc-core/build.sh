a059f531cb7ada0a42f3b7b77e59b0d1cc5dfbb1
krande <kristoffer_andersen@outlook.com>
2023-09-17 15:44:54 +0200

    update pythonocc-core compilation

--- src/pythonocc-core/build.sh
+++ src/pythonocc-core/build.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 # make an in source build do to some problems with install
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*linux.* ]]; then
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-fi
 
 # Configure step
 cmake -G Ninja \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -Wno-dev \
   -DPYTHONOCC_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_LIBRARY_PATH:FILEPATH="$PREFIX/lib" \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
   ${CMAKE_PLATFORM_FLAGS[@]} \
   -DPYTHONOCC_MESHDS_NUMPY=ON \
-  -DCMAKE_PREFIX_PATH=$PREFIX \
-  -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
   -DPython3_FIND_STRATEGY=LOCATION \
   -DPython3_FIND_FRAMEWORK=NEVER \
   -DSWIG_HIDE_WARNINGS=ON \
+  -Wno-dev \
+  -DPYTHONOCC_VERSION=$OCCT_VERSION \
   .
 
 # Build step
@@ -25,10 +23,3 @@ ninja
 
 # Install step
 ninja install
-
-# fix rpaths
-if [ $(uname) == Darwin ]; then
-    for lib in $(ls $SP_DIR/OCC/_*.so); do
-      install_name_tool -rpath $PREFIX/lib @loader_path/../../../ $lib
-    done
-fi
---
+++ src/pythonocc-core/run_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd .test
+python run_tests.py
+mypy test_mypy_classic_occ_bottle.py
