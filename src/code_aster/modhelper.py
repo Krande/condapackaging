7ebcccbe402cd0e145436bb363a63ef4efd05178
krande <kristoffer_andersen@outlook.com>
2023-10-09 14:55:09 +0200

    bump latest Code_Aster

--- src/code_aster/modhelper.py
+++ src/code_aster/modhelper.py
@@ -89,7 +89,7 @@ def set_meta_requirement(req_type, package, version=None, root_dir='.'):
 
 
 if __name__ == '__main__':
-    compiler_version = 13
+    compiler_version = 8
     # This will harmonize all compilers to same version
     change_compilers(compiler_version)
     # set_meta_requirement('build', 'sysroot_linux-64', '2.17')
