c1c5e734f2f07d7f5aa7c27a67771e27a228588e
krande <kristoffer_andersen@outlook.com>
2023-10-31 18:16:28 +0100

    update test results

--- src/code_aster/.tests/check_failed_test.py
+++ src/code_aster/.tests/check_failed_test.py
@@ -1,5 +1,7 @@
 from scan_failed import fail_checker
 
 if __name__ == "__main__":
-    aster_ver = "16.4.9"
-    fail_checker(f'temp/ctest-results-{aster_ver}-ca-6458726549-mpi/ctest-code_aster-linux-3.11', aster_ver, True)
+    aster_ver = "16.4.11"
+    variant = "ca-6706109178-mpi"
+    # variant = "ca-6694568706-mpi"
+    fail_checker(f'temp/ctest-results-{aster_ver}-{variant}/ctest-code_aster-linux-3.11', aster_ver, True)
