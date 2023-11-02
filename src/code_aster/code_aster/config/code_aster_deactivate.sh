2e3d6ae40a36d9b7d956be0daf44ca7398999f1f
krande <kristoffer_andersen@outlook.com>
2023-11-02 16:35:51 +0100

    update ca build script to move code_aster script files and don't rely on activation scripts

--- src/code_aster/code_aster/config/code_aster_deactivate.sh
+++ src/code_aster/code_aster/config/code_aster_deactivate.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Remove paths from PYTHONPATH
-export PYTHONPATH=$(echo $PYTHONPATH | sed "s|${CONDA_PREFIX}/lib/aster:||g")
-export PYTHONPATH=$(echo $PYTHONPATH | sed "s|${CONDA_PREFIX}/lib:||g")
-
-# Remove paths from LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed "s|${CONDA_PREFIX}/lib/aster:||g")
-export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed "s|${CONDA_PREFIX}/lib/petsc4py/lib:||g")
+#export PYTHONPATH=$(echo $PYTHONPATH | sed "s|${CONDA_PREFIX}/lib/aster:||g")
+#export PYTHONPATH=$(echo $PYTHONPATH | sed "s|${CONDA_PREFIX}/lib:||g")
+#
+## Remove paths from LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed "s|${CONDA_PREFIX}/lib/aster:||g")
+#export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed "s|${CONDA_PREFIX}/lib/petsc4py/lib:||g")
 
 # Remove paths from C_INCLUDE_PATH
 export C_INCLUDE_PATH=$(echo $C_INCLUDE_PATH | sed "s|${CONDA_PREFIX}/include/aster:||g")
