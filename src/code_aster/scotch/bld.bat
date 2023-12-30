8823c48c3d38aa81d8755776bcaeddb46de8ac15
krande <kristoffer_andersen@outlook.com>
2023-12-30 22:22:26 +0100

    start to add windows build batch files for scotch

---
+++ src/code_aster/scotch/bld.bat
@@ -0,0 +1,18 @@
+set BISON_PKGDATADIR=%BUILD_PREFIX%\Library\share\winflexbison\data\
+
+cmake ^
+  -G "Ninja" ^
+  -D CMAKE_BUILD_TYPE=Release ^
+  -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -D BUILD_SHARED_LIBS=OFF ^
+  -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
+  -D THREADS_PTHREADS_INCLUDE_DIR="%LIBRARY_INC%" ^
+  -D THREADS_PTHREADS_WIN32_LIBRARY:FILEPATH="%LIBRARY_LIB%\pthread.lib" ^
+  -B build ^
+  %SRC_DIR%
+if errorlevel 1 exit 1
+
+cmake --build ./build --config Release
+if errorlevel 1 exit 1
+cmake --install ./build --component=libscotch
+if errorlevel 1 exit 1
