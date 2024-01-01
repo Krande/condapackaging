ed174184edd413b32b0b7bc16e1a5599b646d926
krande <kristoffer_andersen@outlook.com>
2024-01-01 12:10:11 +0100

    add fixes for code aster build

--- src/code_aster/code_aster/build.sh
+++ src/code_aster/code_aster/build.sh
@@ -104,19 +104,12 @@ sed -i 's/except ImportError:/except ImportError as e:\n    print(f"ImportError:
 # Change the PYTHONPATH just for pybind11_stubgen to find the necessary module
 export PYTHONPATH="$PREFIX/lib/aster:$SRC_DIR/stubgen"
 export LD_LIBRARY_PATH="${PREFIX}/lib/aster"
-# Generate stubs for pybind11
-#$PREFIX/bin/python  ${RECIPE_DIR}/stubs/custom_stubs_gen.py
-#echo "Stubs generation completed"
 
-# This is for reducing reliance on conda activation scripts. But it's not yet working
-mv $PREFIX/lib/aster/code_aster $SP_DIR/code_aster
-mv $PREFIX/lib/aster/run_aster $SP_DIR/run_aster
-# note to self. aster.so is symlinked to libaster.so
-mv $PREFIX/lib/aster/libb*.so $PREFIX/lib/
-mv $PREFIX/lib/aster/libAsterMFrOfficial.so $PREFIX/lib/
-mv $PREFIX/lib/aster/*.so $SP_DIR/
-mv $PREFIX/lib/aster/*.pyi $SP_DIR/
-#mv $PREFIX/include/aster/* $PREFIX/include
+cp -R ${SRC_DIR}/code_aster ${SP_DIR}
+
+# Generate stubs for pybind11
+$PREFIX/bin/python  ${RECIPE_DIR}/stubs/custom_stubs_gen.py
+echo "Stubs generation completed"
 
 # copy modified shell scripts and create backups of the ones we don't want.
 cp $PREFIX/bin/run_aster $PREFIX/bin/_run_aster_old
@@ -125,17 +118,3 @@ cp $PREFIX/bin/run_ctest $PREFIX/bin/_run_ctest_old
 cp $RECIPE_DIR/config/run_aster $PREFIX/bin/run_aster
 cp $RECIPE_DIR/config/run_ctest $PREFIX/bin/run_ctest
 # Update the path to python env root dir in run_aster utils.py
-# sed -i 's|RUNASTER_ROOT = os\.environ\.get(\n    "RUNASTER_ROOT", osp\.dirname(osp\.dirname(osp\.dirname(osp\.dirname(osp\.abspath(__file__)))))\)|RUNASTER_ROOT = os.environ.get(\n    "RUNASTER_ROOT", osp.dirname(osp.dirname(osp.dirname(osp.dirname(osp.dirname(osp.abspath(__file__)))))))|' $PREFIX/bin/run_aster/utils.py
-
-
-# Alternative, I could move the entire code_aster subdirectory to site-packages granted I am able to relocate all
-# relevant .so files
-# Add activation/deactivation scripts to set/unset required env variables for code-aster
-mkdir -p $PREFIX/etc/conda/activate.d
-cp $RECIPE_DIR/config/code_aster_activate.sh $PREFIX/etc/conda/activate.d/code_aster_activate.sh
-chmod +x $PREFIX/etc/conda/activate.d/code_aster_activate.sh
-
-mkdir -p $PREFIX/etc/conda/deactivate.d
-cp $RECIPE_DIR/config/code_aster_deactivate.sh $PREFIX/etc/conda/deactivate.d/code_aster_deactivate.sh
-chmod +x $PREFIX/etc/conda/deactivate.d/code_aster_deactivate.sh
-
