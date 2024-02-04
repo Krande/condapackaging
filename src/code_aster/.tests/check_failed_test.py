4b96416978cfb8984325846e96cd0aa0859c5976
krande <kristoffer_andersen@outlook.com>
2024-02-04 10:53:36 +0100

    try to pin hdf5 for runtime req

--- src/code_aster/.tests/check_failed_test.py
+++ src/code_aster/.tests/check_failed_test.py
@@ -1,8 +1,8 @@
 from scan_failed import fail_checker
 
 if __name__ == "__main__":
-    aster_ver = "16.4.14"
-    channel = "ca-6929259947"
+    aster_ver = "17.0.5"
+    channel = "ca-7761113790"
     variant = "seq"
 
     fail_checker(f'temp/ctest-results-{aster_ver}-{channel}-{variant}/ctest-code_aster-linux-3.11-{variant}', aster_ver,
