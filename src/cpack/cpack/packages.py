3327db84fd1e23ed59659cf82565e72682d83368
krande <kristoffer_andersen@outlook.com>
2024-02-12 20:05:06 +0100

    infer code aster version from meta.yaml

--- src/cpack/cpack/packages.py
+++ src/cpack/cpack/packages.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 
 
@@ -6,3 +7,19 @@ class Package:
     name: str
     version: str
     build: str
+
+
+def get_meta_package_version(meta_yaml_file) -> str:
+    with open(meta_yaml_file, 'r') as file:
+        text = file.read()
+    version_pattern = r"{%\s*set\s+version\s*=\s*['\"](.+?)['\"]\s*%}"
+    match = re.search(version_pattern, text)
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError('Unable to find version number')
+
+
+if __name__ == '__main__':
+    result = get_meta_package_version(r'/home/kristoffer/code/condapackaging/src/code_aster/code_aster/meta.yaml')
+    print(f'{result=}')
