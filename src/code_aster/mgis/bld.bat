0dce5d5b43e305ab8ec3ecece78fb0001ec7470b
krande <kristoffer_andersen@outlook.com>
2024-03-16 17:40:30 +0100

    minor updates

--- src/code_aster/mgis/bld.bat
+++ src/code_aster/mgis/bld.bat
@@ -3,29 +3,11 @@ setlocal
 
 set CLICOLOR_FORCE=1
 
-mkdir build
-cd build
-
 rem Set TFELHOME to the PREFIX environment variable
 set TFELHOME=%PREFIX%
-set python_version=%CONDA_PY:~0,1%.%CONDA_PY:~1,2%
-
-if "%PKG_DEBUG%"=="True" (
-  echo Debugging Enabled
-  set CFLAGS=-g -O0 %CFLAGS%
-  set CXXFLAGS=-g -O0 %CXXFLAGS%
-  set FCFLAGS=-g -O0 %FCFLAGS%
-  set build_type=Debug
-) else (
-  set build_type=Release
-  echo Debugging Disabled
-)
-rem -DCMAKE_CXX_COMPILER=clang-cl ^
-rem -DCMAKE_C_COMPILER=clang-cl ^
-rem -DCMAKE_LINKER=lld-link ^
-rem -DCMAKE_NM=llvm-nm ^
 
 cmake -B build . -G "Ninja" ^
+    %CMAKE_ARGS% ^
     -DCMAKE_BUILD_TYPE=%build_type% ^
     -Denable-c-bindings=OFF ^
     -Denable-fortran-bindings=OFF ^
@@ -37,13 +19,14 @@ cmake -B build . -G "Ninja" ^
     -DPYTHONLIBS_VERSION_STRING="%CONDA_PY%" ^
     -DPython_ADDITIONAL_VERSIONS="%python_version%" ^
     -DPYTHON_EXECUTABLE:FILEPATH="%PREFIX%/python.exe" ^
-    -DPYTHON_LIBRARY:FILEPATH="%PREFIX%/libs/python%python_version%.lib" ^
+    -DPYTHON_LIBRARY:FILEPATH=%PREFIX%\libs\python%CONDA_PY%.lib ^
     -DPYTHON_LIBRARY_PATH:PATH="%PREFIX%/libs" ^
     -DPYTHON_INCLUDE_DIRS:PATH="%PREFIX%/include" ^
     -DUSE_EXTERNAL_COMPILER_FLAGS=ON ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%PREFIX%"
+    -DSITE_PACKAGES_DIR:PATH=%SP_DIR%
 
 cmake --build build --target install
+
 IF ERRORLEVEL 1 (
   type configure.log
   exit /b 1
