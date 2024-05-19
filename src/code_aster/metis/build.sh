3e8b37af96818a79477f6f68c82a7d06384a9f8d
krande <kristoffer_andersen@outlook.com>
2024-05-19 11:00:36 +0200

    update metis for code aster

--- src/code_aster/metis/build.sh
+++ src/code_aster/metis/build.sh
@@ -13,10 +13,24 @@ else
     echo "Debugging Disabled"
 fi
 
+mkdir "content"
+tar -xzf "$SRC_DIR/deps/archives/gklib-master.tar.gz" -C ${SRC_DIR}/content --strip-components=1
+tar -xzf "$SRC_DIR/deps/archives/parmetis-4.0.3_aster3.tar.gz" -C ${SRC_DIR} --strip-components=1
+
+# GKLib
+cd content
+
+make config CFLAGS="-fPIC ${CFLAGS}" prefix=$PREFIX
+make -j ${procs}
+make install
+
+# METIS
+cd ../metis
 # The -fPIC flag is automatically added by conda-build, but I'll add it here just in case
-mkdir -p $PREFIX
-make config prefix=$PREFIX CFLAGS="-fPIC ${CFLAGS}"
-make
+make config CFLAGS="-fPIC ${CFLAGS}" \
+    prefix=${PREFIX} \
+    shared=1 i64=1 r64=1
+make -j ${procs}
 make install
 
 echo "METIS Build complete"
