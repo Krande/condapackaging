7f32868f21377cc96c1ceeb45f9c87efdf7d5018
krande <kristoffer_andersen@outlook.com>
2024-05-22 13:50:55 +0200

    start updating libmed and medcoupling

---
+++ src/code_aster/libmed/bld-ifx.bat
@@ -0,0 +1,42 @@
+@ECHO ON
+
+mkdir build
+cd build
+
+set "INTEL_VARS_PATH=C:\Program Files (x86)\Intel\oneAPI\compiler\latest\env"
+@call "%INTEL_VARS_PATH%\vars.bat" -arch intel64 vs2022
+
+:: This updates the symbols to lowercase and adds an underscore
+xcopy %RECIPE_DIR%\medfwrap_symbols.def %SRC_DIR%\src\medfwrap_symbols.def.in /Y
+
+set FC=ifx
+set FFLAGS=%FFLAGS% /nologo /fpp /fixed /dll /MD /real-size:64 /integer-size:64
+
+cmake -G "Ninja" ^
+  %CMAKE_ARGS% ^
+  -D Python_FIND_STRATEGY:STRING=LOCATION ^
+  -D Python_FIND_REGISTRY:STRING=NEVER ^
+  -D Python3_ROOT_DIR:FILEPATH="%PREFIX%" ^
+  -D HDF5_ROOT_DIR:FILEPATH="%LIBRARY_PREFIX%" ^
+  -D MEDFILE_INSTALL_DOC=OFF ^
+  -D MEDFILE_BUILD_PYTHON=ON ^
+  -D MEDFILE_BUILD_TESTS=OFF ^
+  -D MEDFILE_BUILD_SHARED_LIBS=ON ^
+  -D MEDFILE_BUILD_STATIC_LIBS=OFF ^
+  -D MEDFILE_USE_UNICODE=OFF ^
+  -D MED_MEDINT_TYPE=long ^
+  -Wno-dev ^
+  ..
+
+if errorlevel 1 exit 1
+ninja
+if errorlevel 1 exit 1
+mkdir %SP_DIR%\med
+if errorlevel 1 exit 1
+ninja install
+if errorlevel 1 exit 1
+
+copy %LIBRARY_BIN%\mdump4.exe %LIBRARY_BIN%\mdump.exe
+if errorlevel 1 exit 1
+copy %LIBRARY_BIN%\xmdump4 %LIBRARY_BIN%\xmdump
+if errorlevel 1 exit 1
