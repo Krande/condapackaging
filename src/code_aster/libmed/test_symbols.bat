b9b8b6702336ff21a3e7893f1e414a47d9f5eaaa
krande <kristoffer_andersen@outlook.com>
2024-05-22 16:29:48 +0200

    update common.yaml and fix medcoupling and libmed and test conda batch ci

---
+++ src/code_aster/libmed/test_symbols.bat
@@ -0,0 +1,16 @@
+@echo off
+SET DLL_PATH="%LIBRARY_PREFIX%\bin\medfwrap.dll"
+if "%LIBRARY_PREFIX%"=="" (
+    echo Error: LIBRARY_PREFIX is not set.
+    SET DLL_PATH="%CONDA_PREFIX%\Library\bin\medfwrap.dll"
+)
+
+SET SYMBOL=MSEIPW
+
+dumpbin /EXPORTS %DLL_PATH% | findstr %SYMBOL%
+IF ERRORLEVEL 1 (
+    echo Error: Symbol %SYMBOL% not found in %DLL_PATH%.
+    exit /b 1
+) ELSE (
+    echo Success: Symbol %SYMBOL% found in %DLL_PATH%.
+)
