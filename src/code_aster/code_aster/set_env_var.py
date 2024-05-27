6c37a80c7d0fdd87bbc4efaafdde3d6fa01724a8
krande <kristoffer_andersen@outlook.com>
2024-05-27 09:15:20 +0200

    also print the bat file

--- src/code_aster/code_aster/set_env_var.py
+++ src/code_aster/code_aster/set_env_var.py
@@ -4,6 +4,9 @@ import argparse
 
 
 def set_env(name, value):
+    if "GITHUB_ENV" not in os.environ:
+        return
+
     with open(os.environ["GITHUB_ENV"], "a") as fh:
         print(f"{name}={value}", file=fh)
 
@@ -12,5 +15,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("build_dir", type=str)
     args = parser.parse_args()
+
     build_dir = pathlib.Path(args.build_dir).resolve().absolute()
     set_env('CONDA_BUILD_DIR', build_dir.as_posix())
