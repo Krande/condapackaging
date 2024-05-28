54f1ca8670d2efe7e72885b0c303855084d852ab
krande <kristoffer_andersen@outlook.com>
2024-05-28 09:48:46 +0200

    update path cleaner

--- src/code_aster/code_aster/config/path_cleaner.py
+++ src/code_aster/code_aster/config/path_cleaner.py
@@ -8,16 +8,33 @@ def main():
         "NETFX",
         "HTML Help Workshop",
         "Team Explorer",
-        "Roslyn"
+        "Roslyn",
+        "Common7",
+        "llvm",
+        "WindowsApps",
+        ".dotnet",
+        ".cargo",
+        "Amazon"
+        "php",
+        "Microsoft Service Fabric",
+        "Strawberry",
+        "Microsoft SQL Server",
+        "WiX Toolset",
+        "kotlinc",
     ]
-    new_path = []
+    new_path = set()
     for path in os.getenv("PATH").split(os.pathsep):
+        should_skip = False
         for filter_path in filter_paths_containing:
             if filter_path.lower() in path.lower():
-                print(f"Skipping {path}")
-                continue
+                should_skip = True
+                break
+
+        if should_skip:
+            print(f"Skipping {path}")
+        else:
             print(f"Adding {path}")
-        new_path.append(path)
+            new_path.add(path)
 
     os.environ["PATH"] = os.pathsep.join(new_path)
 
