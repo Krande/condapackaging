582a7e0778ec6738f5aea150c4669a24832f689f
krande <kristoffer_andersen@outlook.com>
2024-06-13 14:28:38 +0200

    revert intsize specification in metis

--- src/code_aster/metis/bld.bat
+++ src/code_aster/metis/bld.bat
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-set CMAKE_BUILD_TYPE=Release
+set BUILD_TYPE=Release
 if "%build_type%" == "debug" (
     set CMAKE_BUILD_TYPE=Debug
     set CFLAGS=%CFLAGS% /Od /Z7
@@ -13,10 +13,10 @@ cmake ^
     -G "Ninja" ^
     -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -D CMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
+    -D CMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -D SHARED=ON ^
-    -D IDXTYPEWIDTH=64 ^
-    -D REALTYPEWIDTH=64 ^
+    -D intsize=64 ^
+    -D realsize=64 ^
     ..
 
 if errorlevel 1 exit 1
