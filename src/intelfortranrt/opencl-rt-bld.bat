a0bc980bbdb1f079ba7e1f7054a7bd8c3961cee1
krande <kristoffer_andersen@outlook.com>
2024-06-20 16:09:27 +0200

    add intel fortran rt build to devtools

---
+++ src/intelfortranrt/opencl-rt-bld.bat
@@ -0,0 +1,14 @@
+@echo on
+if not exist "%LIBRARY_BIN%\intel-ocl-cpu" mkdir "%LIBRARY_BIN%\intel-ocl-cpu"
+set "src1=%SRC_DIR%\%PKG_NAME%\Library\bin"
+robocopy /E "%src1%" "%LIBRARY_BIN%\intel-ocl-cpu"
+if %ERRORLEVEL% GEQ 8 exit 1
+
+set "src2=%SRC_DIR%\%PKG_NAME%\Library\lib"
+robocopy /E "%src2%" "%LIBRARY_LIB%"
+if %ERRORLEVEL% GEQ 8 exit 1
+
+:: populate CL_CONFIG_TBB_DLL_PATH = entry in cl.cfg setting it to %LIBRARY_BIN%
+python %RECIPE_DIR%\set_tbb_dll_path.py %LIBRARY_BIN%\intel-ocl-cpu\cl.cfg
+mkdir "%LIBRARY_PREFIX%\etc\OpenCL\vendors\"
+echo %LIBRARY_BIN%\intel-ocl-cpu\intelocl64.dll> %LIBRARY_PREFIX%\etc\OpenCL\vendors\intel-ocl-cpu.icd
---
+++ src/intelfortranrt/repack.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ex
+
+# for subpackages, we have named our extracted locations according to the subpackage name
+#    That's what this $PKG_NAME is doing - picking the right subfolder to rsync
+
+src="${SRC_DIR}/${PKG_NAME}"
+
+cp -av "${src}"/* "${PREFIX}/"
+
+# replace old info folder with our new regenerated one
+rm -rf "${PREFIX}/info"
+
+if [[ "$PKG_NAME" == "intel-fortran-rt" && "$target_platform" == "linux-"* ]]; then
+  for f in "libicaf.so" "libifcoremt.so.5" "libifcore.so.5" "libifport.so.5"; do
+    # Remove RUNPATH and keep only RPATH
+    patchelf --set-rpath '$ORIGIN' --force-rpath ${PREFIX}/lib/$f
+  done
+fi
+
+if [[ "$PKG_NAME" == "dpcpp_impl_linux-64" ]]; then
+  # Not sure why these libraries are there, but seems to be unused.
+  rm -f $PREFIX/lib/libffi.so*
+  HOST=x86_64-conda-linux-gnu
+  echo "--gcc-toolchain=${PREFIX} --sysroot=${PREFIX}/${HOST}/sysroot -target ${HOST}" > ${PREFIX}/bin/$HOST-icpx.cfg
+  echo "--gcc-toolchain=${PREFIX} --sysroot=${PREFIX}/${HOST}/sysroot -target ${HOST}" > ${PREFIX}/bin/$HOST-icx.cfg
+fi
