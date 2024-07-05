8c867c01d61cfecae5d30f6069a1deb91280fc55
krande <kristoffer_andersen@outlook.com>
2024-07-05 16:00:14 +0200

    further work on adding shared lib support for metis and mumps

--- src/code_aster/metis/bld.bat
+++ src/code_aster/metis/bld.bat
@@ -6,7 +6,7 @@ cd build
 set TGT_BUILD_TYPE=Release
 if "%build_type%" == "debug" (
     set TGT_BUILD_TYPE=Debug
-    set CFLAGS=%CFLAGS% /Od /Z7
+    set CFLAGS=%CFLAGS% /nologo /Od /Zi
     set LDFLAGS=%LDFLAGS% /DEBUG /INCREMENTAL:NO
 )
 
@@ -15,7 +15,10 @@ cmake ^
     -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -D CMAKE_BUILD_TYPE=%TGT_BUILD_TYPE% ^
-    -D SHARED=ON ^
+    -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+    -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS:BOOL=ON ^
+    -D BUILD_TESTING:BOOL=ON ^
+    -D BUILD_SHARED_LIBS=OFF ^
     -D intsize=64 ^
     -D realsize=64 ^
     ..
