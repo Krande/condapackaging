d1e91e2530a4c6f10815acdf81623daabb9c35e6
krande <kristoffer_andersen@outlook.com>
2024-07-24 10:53:17 +0200

    slight modification of hdf5 to produce true debug builds

--- src/code_aster/hdf5/run_test.bat
+++ src/code_aster/hdf5/run_test.bat
@@ -1,3 +1,48 @@
 @echo off
 
-REM todo: add checking of symbols tests for windows here
+rem Verify CLI tools.
+setlocal enabledelayedexpansion
+
+set hdf5_cmds=gif2h5 h52gif h5copy h5debug h5diff h5dump h5import h5jam h5ls h5mkgrp h5repack h5repart h5stat h5unjam
+
+if errorlevel 1 exit 1
+
+for %%C in (%hdf5_cmds%) do (
+    echo %%C
+	where %%C || (
+		echo Command not found: %%C
+		exit /b 1
+	)
+)
+
+if errorlevel 1 exit 1
+
+rem Verify libraries.
+set hdf5_libs=hdf5 hdf5_cpp hdf5_hl hdf5_hl_cpp
+echo "build_type: %build_type%"
+
+for %%L in (%hdf5_libs%) do (
+    if "%build_type%" == "debug" (
+        if not exist %PREFIX%\Library\lib\%%L_D.lib (
+           echo Library not found: %PREFIX%\Library\lib\%%L_D.lib
+            exit /b 1
+        )
+        if not exist %PREFIX%\Library\bin\%%L_D.dll (
+            echo Library not found: %PREFIX%\Library\bin\%%L_D.dll
+            exit /b 1
+        )
+    ) else (
+        if not exist %PREFIX%\Library\lib\%%L.lib (
+           echo Library not found: %PREFIX%\Library\lib\%%L.lib
+            exit /b 1
+        )
+        if not exist %PREFIX%\Library\bin\%%L.dll (
+            echo Library not found: %PREFIX%\Library\bin\%%L.dll
+            exit /b 1
+        )
+    )
+)
+
+if errorlevel 1 exit 1
+
+endlocal
--- src/code_aster/hdf5/run_test.sh
+++ src/code_aster/hdf5/run_test.sh
@@ -1,40 +1,32 @@
-# Stop on first error
-set -euxo pipefail
+#!/bin/bash
 
-# test dir populated from recipe/test
-pushd test
-
-# Test C compiler
-if [[ "$mpi" != "nompi" ]]; then
-    h5cc=h5pcc
-    h5fc=h5pfc
+# Verify UNIX CLI tools.
+if [ "${mpi}" == "nompi" ]; then
+    h5_compilers=("h5c++" "h5cc" "h5fc")
 else
-    h5cc=h5cc
-    h5fc=h5fc
+    h5_compilers=("h5c++" "h5pcc" "h5pfc")
 fi
-echo "Testing $h5cc"
-$h5cc -showconfig
-$h5cc ${CFLAGS} ${LDFLAGS} h5_cmprss.c -o h5_cmprss
-./h5_cmprss
 
-# Test C++ compiler
-echo "Testing h5c++"
-h5c++ -showconfig
-h5c++ ${CXXFLAGS} ${LDFLAGS} h5tutr_cmprss.cpp -o h5tutr_cmprss
-./h5tutr_cmprss
+hdf5_unix_cmds=("${h5_compilers[@]}" "h5redeploy")
 
-# Test Fortran 90 compiler
-echo "Testing $h5fc"
-$h5fc -showconfig
-$h5fc ${FFLAGS} ${LDFLAGS} h5_cmprss.f90 -o h5_cmprss
-./h5_cmprss
+if [ "${build_platform}" == "${target_platform}" ]; then
+    hdf5_unix_cmds+=("h5perf_serial")
+fi
 
-# Test Fortran 2003 compiler, note that the file has a 90 extension
-echo "Testing h5fc for Fortran 2003"
-$h5fc ${FFLAGS} ${LDFLAGS} compound_fortran2003.f90 -o compound_fortran2003
-./compound_fortran2003
+for each_hdf5_unix_cmd in "${hdf5_unix_cmds[@]}"; do
+    command -v "${each_hdf5_unix_cmd}" || exit 1
+done
 
-echo "Testing cmake"
-cmake -B build .
-cmake --build build
-./build/h5_cmprss
+# Verify CLI tools.
+hdf5_cmds=("gif2h5" "h52gif" "h5copy" "h5debug" "h5diff" "h5dump" "h5import" "h5jam" "h5ls" "h5mkgrp" "h5repack" "h5repart" "h5stat" "h5unjam")
+
+for each_hdf5_cmd in "${hdf5_cmds[@]}"; do
+    command -v "${each_hdf5_cmd}" || exit 1
+done
+
+# Verify libraries.
+hdf5_libs=("hdf5" "hdf5_cpp" "hdf5_hl" "hdf5_hl_cpp")
+
+for each_hdf5_lib in "${hdf5_libs[@]}"; do
+    test -f "$PREFIX/lib/lib${each_hdf5_lib}${SHLIB_EXT}" || exit 1
+done
