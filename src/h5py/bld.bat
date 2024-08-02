d1947b2f31bfd2b24ed874f237b87ebf5e2e3b0e
krande <kristoffer_andersen@outlook.com>
2024-08-02 15:11:50 +0200

    further work on making a ci tool for manually updating and aligning adapy dependencies

--- src/h5py/bld.bat
+++ src/h5py/bld.bat
@@ -4,5 +4,12 @@ set HDF5_DIR="%LIBRARY_PREFIX%"
 REM tell setup.py to not 'pip install' exact package requirements
 set H5PY_SETUP_REQUIRES=0
 
+@REM dir "%RECIPE_DIR%/config"
+@REM dir "%SRC_DIR%"
+
+copy /Y "%RECIPE_DIR%\config\setup_configure.py" "%SRC_DIR%\setup_configure.py"
+copy /Y "%RECIPE_DIR%\config\version.py" "%SRC_DIR%\h5py\version.py"
+copy /Y "%RECIPE_DIR%\config\__init__.py" "%SRC_DIR%\h5py\__init__.py"
+
 "%PYTHON%" -m pip install . --no-deps --ignore-installed --no-cache-dir -vv
 if errorlevel 1 exit 1
