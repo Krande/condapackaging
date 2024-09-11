ebd91e4f90011bf55b79e337be578c22b6e55765
krande <kristoffer_andersen@outlook.com>
2024-09-11 10:46:27 +0200

    have a working debug build for occt

--- src/occt/bld.bat
+++ src/occt/bld.bat
@@ -1,9 +1,14 @@
 set TGT_BUILD_TYPE=Release
+set USE_TBB=OFF
+set USE_MMGR_TYPE=""
 if "%build_type%" == "debug" (
     set TGT_BUILD_TYPE=Debug
     set CFLAGS=%CFLAGS% /Od /Zi
     set CXXFLAGS=%CFLAGS% /Od /Zi
     set LDFLAGS=%LDFLAGS% /DEBUG /INCREMENTAL:NO
+    :: for some reason, debug builds results in memory leaks without an alternative memory manager
+    set USE_TBB=ON
+    set USE_MMGR_TYPE=TBB
 )
 
 if "%variant%" == "novtk" (
@@ -20,15 +25,14 @@ cmake -S . -B build  -G Ninja ^
       -D BUILD_MODULE_Draw=OFF ^
       -D 3RDPARTY_DIR:FILEPATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_BUILD_TYPE="%TGT_BUILD_TYPE%" ^
-      -D USE_TBB=OFF ^
-      -D BUILD_RELEASE_DISABLE_EXCEPTIONS=OFF ^
+      -D USE_TBB=%USE_TBB% ^
+      -D USE_MMGR_TYPE=%USE_MMGR_TYPE% ^
       -D USE_VTK:BOOL=%USE_VTK% ^
       -D 3RDPARTY_VTK_LIBRARY_DIR:FILEPATH="%LIBRARY_PREFIX%/lib" ^
       -D 3RDPARTY_VTK_DLL_DIR:FILEPATH="%LIBRARY_PREFIX%/bin" ^
       -D 3RDPARTY_VTK_INCLUDE_DIR:FILEPATH="%LIBRARY_PREFIX%/include/vtk-9.3" ^
+      -D 3RDPARTY_TBB_DIR:FILEPATH="%LIBRARY_PREFIX%" ^
       -D VTK_RENDERING_BACKEND:STRING="OpenGL2" ^
-      -D GLEW_LIBRARY:FILEPATH="%LIBRARY_PREFIX%/lib/glew32.lib" ^
-      -D TBB_LIBRARY_RELEASE:FILEPATH="%LIBRARY_PREFIX%/lib/tbb.lib" ^
       -D USE_FREEIMAGE:BOOL=ON ^
       -D USE_RAPIDJSON:BOOL=ON ^
       -D BUILD_RELEASE_DISABLE_EXCEPTIONS:BOOL=OFF
