e7b5e149fd21e607d29a69d6f43391557b5e6f29
krande <kristoffer_andersen@outlook.com>
2024-10-30 14:00:50 +0100

    simplify fortran installer

---
+++ .github/actions/install-intel-fortran/install-intel-fortran.bat
@@ -0,0 +1,2 @@
+wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/2bff9d18-ac2b-40b1-8167-00156f466b0e/intel-fortran-essentials-2025.0.0.301_offline.exe
+intel-fortran-essentials-2025.0.0.301_offline.exe -s -a --silent --eula accept -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0
