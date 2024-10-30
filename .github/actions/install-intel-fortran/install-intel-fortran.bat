8172e214f2033707382d947198678e2f70e71962
krande <kristoffer_andersen@outlook.com>
2024-10-30 14:03:02 +0100

    use curl not wget

--- .github/actions/install-intel-fortran/install-intel-fortran.bat
+++ .github/actions/install-intel-fortran/install-intel-fortran.bat
@@ -1,2 +1,8 @@
-wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/2bff9d18-ac2b-40b1-8167-00156f466b0e/intel-fortran-essentials-2025.0.0.301_offline.exe
+:: Download installer
+curl.exe --url https://registrationcenter-download.intel.com/akdlm/IRC_NAS/2bff9d18-ac2b-40b1-8167-00156f466b0e/intel-fortran-essentials-2025.0.0.301_offline.exe --output intel-fortran-essentials-2025.0.0.301_offline.exe
+
+:: Run the installer in silent mode
 intel-fortran-essentials-2025.0.0.301_offline.exe -s -a --silent --eula accept -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0
+
+:: Clean up
+del intel-fortran-essentials-2025.0.0.301_offline.exe
