0cee990ae09fae8cf9bb3862ac6febd25b605c8c
krande <kristoffer_andersen@outlook.com>
2025-10-31 10:42:46 +0100

    update build configuration in recipe.yaml and remove dependency from pixi.toml

--- src/code_aster/code_aster/activate_intel.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-call "C:\Program Files (x86)\Intel\oneAPI\compiler\latest\env\vars.bat" -arch intel64
-if errorlevel 1 (
-    echo "Failed to activate Intel environment."
-    exit /b 1
-)
-@REM where ifx.exe
-if errorlevel 1 exit /b 1
